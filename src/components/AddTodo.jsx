66216fa52c04d798a348b3bb54a8a01b8834b67e
reflevius <nikita.l@webspace.by>
2022-07-19 15:05:56 +0300

    feat: todos

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -4,12 +4,32 @@ import {
   FormControl,
   Input,
 } from '@chakra-ui/react';
-
+import { useMutation } from '@apollo/client';
+import {ADD_TODO, ALL_TODO} from '../apollo/todos';
 const AddTodo = () => {
   const [text, setText] = useState('');
-
+  const [addTodo, {error}] = useMutation(ADD_TODO,
+      {
+        // refetchQueries: [ {query: ALL_TODO} ]
+        update(cache, { data: { newTodo } }) {
+          const { todos } = cache.readQuery({ query: ALL_TODO });
+          cache.writeQuery({
+            query: ALL_TODO,
+            data: {
+              todos: [newTodo, ...todos]
+            }
+          })
+        }
+      });
   const handleAddTodo = () => {
     if (text.trim().length) {
+      addTodo({
+        variables: {
+          title: text,
+          userId: 123,
+          completed: false
+        }
+      })
       setText('');
     }
   }
@@ -17,7 +37,9 @@ const AddTodo = () => {
   const handleKey = (event) => {
     if (event.key === "Enter") handleAddTodo();
   }
-
+  if (error) {
+    return <h2>Error...</h2>
+  }
   return (
     <FormControl display={'flex'} mt={6}>
       <Input
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
+import {ChakraProvider} from '@chakra-ui/react';
+import {ApolloProvider} from '@apollo/client';
 
+import client from './apollo/client';
 import App from './App';
 import './index.css';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <ChakraProvider>
+            <ApolloProvider client={client}>
+                <App />
+            </ApolloProvider>
+        </ChakraProvider>
+    </React.StrictMode>
 );
